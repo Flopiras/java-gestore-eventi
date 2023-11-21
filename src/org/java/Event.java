6d657e8d224ca8eaca906c0a6fca98d5aaf11e1c
Flopiras <Flopiras@yahoo.it>
2023-11-21 12:34:46 +0100

    fix bugs

--- src/org/java/Event.java
+++ src/org/java/Event.java
@@ -1,11 +1,7 @@
 package org.java;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.Locale;
+import java.time.format.DateTimeFormatter;
 
 public class Event {
 
@@ -37,11 +33,8 @@ public class Event {
 
 	public void setDate(String date) throws Exception {
 
-		LocalDate today = LocalDate.now();
-		LocalDate eventDate = LocalDate.parse(date);
-		
 		// control
-		if (eventIsExpired())
+		if (eventIsExpired(date))
 			throw new Exception("La data è già passata");
 
 		this.date = date;
@@ -65,29 +58,14 @@ public class Event {
 	}
 
 	// METHODS
-	public String getFormatDate(String inputDate) throws ParseException {
+	public String getFormatDate(String date) {
 
-		SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
-		try {
-
-			Date date = inputDateFormat.parse(inputDate);
-
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-
-		Locale locale = new Locale("it", "IT");
-		DateFormat outputDateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
-
-		String formattedDate = outputDateFormat.format(date);
-
-		System.out.println(formattedDate);
-
-		return formattedDate;
+		LocalDate localDate = LocalDate.parse(date);
+        return localDate.format(DateTimeFormatter.ofPattern("dd/LLLL/yyyy"));
+			
 	}
 	
-	public boolean eventIsExpired() {
+	public boolean eventIsExpired(String date) {
 		
 		LocalDate today = LocalDate.now();
 		LocalDate eventDate = LocalDate.parse(date);
@@ -98,18 +76,25 @@ public class Event {
 	
 	public void prenota() throws Exception {
 		
-		if (eventIsExpired() || seatsCapacity < reservedSeats)
+		if (eventIsExpired(date) || seatsCapacity < reservedSeats)
 			throw new Exception("Impossibile prenotare i posti");
 		
 		reservedSeats += 1;	
 	}
 	
 	public void disdici() throws Exception {
-		if (eventIsExpired() || reservedSeats <= 0)
+		if (eventIsExpired(date) || reservedSeats <= 0)
 			throw new Exception("Non ci sono prenotazioni da disdire");
 		
 		reservedSeats -= 1;
 	}
 	
+	//overrides
+	@Override
+	public String toString() {
+		
+		
+		return getFormatDate(date) + " - " + getTitle();
+	}
 
 }
--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,8 +1,10 @@
 package org.java;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
     	
+    	Event e = new Event("test", "2023-11-25", 12);
     	
+    	System.out.println(e);
     }
 }
