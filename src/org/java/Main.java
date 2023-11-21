0e06b738dc83d4f353a10966353eaadf79762897
Flopiras <Flopiras@yahoo.it>
2023-11-21 12:49:55 +0100

    created new event with scanner

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,10 +1,33 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
     	
-    	Event e = new Event("test", "2023-11-25", 12);
+    	//open Scanner
+    	Scanner in = new Scanner(System.in);
     	
-    	System.out.println(e);
+    	try {
+    		System.out.println("Nome evento: ");
+    		String title = in.nextLine();
+    		
+    		System.out.println("Data evento (yyyy-mm-dd): ");
+    		String date = in.nextLine();
+    		
+    		System.out.println("Numero di posti totale: ");
+    		String strSeatcCapacity = in.nextLine();
+    		int seatCapacity = Integer.valueOf(strSeatcCapacity);
+    		
+    		Event e = new Event(title, date, seatCapacity);
+    		
+    		System.out.println(e.toString());
+    		
+    	} catch (Exception e) {
+    		System.out.println("ERRORE: " + e.getMessage());
+    	}
+    	
+    	//close Scanner
+    	in.close();
     }
 }
