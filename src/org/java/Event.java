acaa445aa5e00f8c33460bdb7aa345716efe8939
Flopiras <Flopiras@yahoo.it>
2023-11-21 11:38:36 +0100

    added exception in date

--- src/org/java/Event.java
+++ src/org/java/Event.java
@@ -1,16 +1,23 @@
 package org.java;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
+
 public class Event {
 
 	private String title;
 	private String date;
 	private int seatsCapacity;
 	private int reservedSeats;
-	
-	public Event(String title, String date, int seatsCapacity) {
-		
+
+	public Event(String title, String date, int seatsCapacity) throws Exception {
+
 		reservedSeats = 0;
-		
+
 		setTitle(title);
 		setDate(date);
 		setSeatsCapacity(seatsCapacity);
@@ -28,7 +35,15 @@ public class Event {
 		return date;
 	}
 
-	public void setDate(String date) {
+	public void setDate(String date) throws Exception {
+
+		LocalDate today = LocalDate.now();
+		LocalDate eventDate = LocalDate.parse(date);
+		
+		// control
+		if (eventDate.isAfter(today))
+			throw new Exception("La data è già passata");
+
 		this.date = date;
 	}
 
@@ -47,6 +62,28 @@ public class Event {
 	public void setReservedSeats(int reservedSeats) {
 		this.reservedSeats = reservedSeats;
 	}
-	
-	
+
+	// METHODS
+	public String getFormatDate(String inputDate) throws ParseException {
+
+		SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+
+			Date date = inputDateFormat.parse(inputDate);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		Locale locale = new Locale("it", "IT");
+		DateFormat outputDateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+
+		String formattedDate = outputDateFormat.format(date);
+
+		System.out.println(formattedDate);
+
+		return formattedDate;
+	}
+
 }
